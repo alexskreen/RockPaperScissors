138fe9054903d9d07dd8bbd401f0394d3a8311b1
Alex Skreen and Jose Amesquita <alex+jose@>
2020-03-04 15:53:26 -0800

    add typline and text color

--- RockPaperScissors/Models/RockPaperScissors.cs
+++ RockPaperScissors/Models/RockPaperScissors.cs
@@ -6,11 +6,15 @@ namespace RockPaperScissors.Models
     {
         public string Player1;
         public string Player2;
+
+        // public string UserGameChoice;
         
         public Game(string player1, string player2)
         {
             Player1 = player1;
             Player2 = player2;
+            // UserGameChoice = "";
+
         }
 
         public string FindWinner()
@@ -20,29 +24,13 @@ namespace RockPaperScissors.Models
             {
             gameWinner = "Try again!";
             }
-            else if (Player1 == "rock" && Player2 == "paper")
+            else if (Player1 == "rock" && Player2 == "paper" || Player2 == "rock" && Player1 == "scissors" || Player2 == "scissors" && Player1 == "paper")
             {
             gameWinner = "Player 2 wins!";
             }
-            else if (Player2 == "rock" && Player1 == "paper")
+            else if (Player2 == "rock" && Player1 == "paper" || Player1 == "rock" && Player2 == "scissors" || Player1 == "scissors" && Player2 == "paper")
             {
             gameWinner = "Player 1 wins!";
-            }
-            else if (Player1 == "rock" && Player2 == "scissors")
-            {
-            gameWinner = "Player 1 wins!";
-            }
-            else if (Player2 == "rock" && Player1 == "scissors")
-            {
-            gameWinner = "Player 2 wins!";
-            }
-            else if (Player1 == "scissors" && Player2 == "paper")
-            {
-            gameWinner = "Player 1 wins!";
-            }
-            else if (Player2 == "scissors" && Player1 == "paper")
-            {
-            gameWinner = "Player 2 wins!";
             }
             return gameWinner;
         }
--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using RockPaperScissors.Models;
 namespace RockPaperScissors.Program
 {
@@ -6,6 +7,8 @@ namespace RockPaperScissors.Program
     {
         public static void Main()
         {
+            Console.ResetColor();
+            Console.Clear();
             Program.ChooseGame();
         }
 
@@ -30,22 +33,31 @@ namespace RockPaperScissors.Program
 
         public static void readResultsPVC(string userInput, string result, Game newGame)
         {
-            Console.WriteLine("Player 1 chose: " + userInput);
-            Console.WriteLine("computer chose: " + result);
-            Console.WriteLine("------------------------");
-            Console.WriteLine(newGame.FindWinner());
+            Console.Clear();
+            // Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(Environment.NewLine);
+            Program.TypeLine("Player 1 chose: " + userInput);
+            Program.TypeLine("computer chose: " + result);
+            Program.TypeLine("------------------------");
+            Program.TypeLine(newGame.FindWinner());
+            Console.ResetColor();
         }
         public static void readResultsPVP(string userInput, string password, Game newGame)
         {
-            Console.WriteLine("Player 1 chose: " + userInput);
-            Console.WriteLine("Player 2 chose: " + password);
-            Console.WriteLine("------------------------");
-            Console.WriteLine(newGame.FindWinner());
+            // Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(Environment.NewLine);
+            Program.TypeLine("Player 1 chose: " + userInput);
+            Program.TypeLine("Player 2 chose: " + password);
+            Program.TypeLine("------------------------");
+            Program.TypeLine(newGame.FindWinner());
+            Console.ResetColor();
         }
 
         public static void ChooseGame()
         {
-            Console.WriteLine("Would you like to play PvP, or PvC?");
+            Program.TypeLine("Would you like to play PvP, or PvC?");
             string userGameChoice = Console.ReadLine().ToLower();
             if (userGameChoice == "pvc")
             {
@@ -59,16 +71,21 @@ namespace RockPaperScissors.Program
 
         public static void PlayerVsComputer()
         {
-            Console.WriteLine("Player 1 enter rock, paper, or scissors");
+            // Console.Clear();
+            // Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(Environment.NewLine);
+            Program.TypeLine("Player 1 enter rock, paper, or scissors");
             string userInput = Console.ReadLine();
-            string result = Program.numberGenerator();
-            Game newGame = new Game(userInput, result);
-            Program.readResultsPVC(userInput, result, newGame);
+            Program.CheckWordAgainstlist(userInput);
         }
 
         public static void PlayerVsPlayer()
         {
-            Console.WriteLine("Player 1 enter rock, paper, or scissors");
+            // Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(Environment.NewLine);
+            Program.TypeLine("Player 1 enter rock, paper, or scissors");
             string password1 = null;
             while (true)
             {
@@ -77,8 +94,9 @@ namespace RockPaperScissors.Program
                     break;
                 password1 += key.KeyChar;
             }
-            // string userInput = Console.ReadLine();
-            Console.WriteLine("Player 2 enter rock, paper, or scissors");
+            Program.CheckWordAgainstlist(password1);
+
+            Program.TypeLine("Player 2 enter rock, paper, or scissors");
             string password2 = null;
             while (true)
             {
@@ -87,9 +105,40 @@ namespace RockPaperScissors.Program
                     break;
                 password2 += key.KeyChar;
             }
-            Console.WriteLine("------------------------");
+            Program.CheckWordAgainstlist(password1);
+            Program.TypeLine("------------------------");
             Game newGame = new Game(password1, password2);
             Program.readResultsPVP(password1, password2, newGame);
+        }
+        public static void TypeLine(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.Write(input[i]);
+                System.Threading.Thread.Sleep(25);
+            }
+            Console.Write(Environment.NewLine);
+            Console.Write(Environment.NewLine);
+        }
+
+        public static void CheckWordAgainstlist(string userInput)
+        {
+            List<string> possibleResponses = new List<string> { "rock", "paper", "scissors" };
+            // string userInput = Console.ReadLine();
+            if (!possibleResponses.Contains(userInput))
+            {
+                Console.Write(Environment.NewLine);
+                Program.TypeLine("No, you must enter 'rock', 'paper' or 'scissors'!");
+                Program.PlayerVsComputer();
+            }
+            else
+            {
+                Console.Clear();
+                string result = Program.numberGenerator();
+                Game newGame = new Game(userInput, result);
+                Program.readResultsPVC(userInput, result, newGame);
+                Console.ResetColor();
+            }
         }
     }
 }
